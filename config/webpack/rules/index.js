0ccf5c9099ce0bc0b92814686fe3dadacf39157f
fixme0 <podolskij.web@gmail.com>
2020-06-15 09:08:22 +0300

    remove source code

--- config/webpack/rules/index.js
+++
@@ -1,4 +0,0 @@
-export { getJsRules } from './jsRules';
-export { getStyleRules } from './styleRules';
-export { getFilesRules } from './filesRules.js';
-export { getTemplateRules } from './templateRules';
